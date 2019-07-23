3a2cf31b121c233db11bbff0894cd31d11381098
Maestro Kenobi <mretrololoshka@gmail.com>
2019-07-23 07:44:50 +0300

    OpenGL development start

---
+++ cpp_dev/boost_libs.h
@@ -0,0 +1,10 @@
+#pragma once
+//ifndef BOOST_LIB
+//define BOOST_LIB
+//
+//include <boost/align.hpp>
+//include <boost/thread.hpp>
+////include <boost/>
+//
+//
+//endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
 #include "stl.h"
 #include "cpp_dev/devlibs.h"
+#include "glew/glew.h"
+#include "freeglut/GL/freeglut.h"
+#include "GLFW/glfw3.h"
+#ifdef _WIN32
+#include "Windows.h"
+#endif
 
-int main() {
-	system("pause");
+void render(void);
+
+int height = GetSystemMetrics(SM_CXSCREEN);
+int width = GetSystemMetrics(SM_CYSCREEN);
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0,0);
+	glutInitWindowSize(height, width);
+	glutCreateWindow("Example");	
+	glutDisplayFunc(render);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+#ifdef _WIN32
+	HWND Console;
+	AllocConsole();
+	Console = FindWindowA("ConsoleWindowClass", NULL);
+	ShowWindow(Console, 0);
+#endif
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+	glutMainLoop();
+
 	return 0;
+}
+
+void render(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
 }
